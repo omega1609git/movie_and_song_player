1d6562034ff9b02b21a45f2ac0b12385563228ee
omega1609git <74846791+omega1609git@users.noreply.github.com>
2023-10-07 12:36:12 +0530

    setup

---
+++ src/store/index.js
@@ -0,0 +1,33 @@
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+
+const songsSlice= createSlice({
+    name:'song',
+    initialState: [],
+    reducers:{
+        addSong(state,action){
+            state.push(action.payload);
+        },
+        removeSong(state,action){
+            //
+        }
+    }
+});
+
+const store = configureStore({
+    reducer:{
+        songs: songsSlice.reducer
+    }
+});
+
+console.log('store->index.js');
+const startingState = store.getState();
+console.log(JSON.stringify(startingState));
+
+
+store.dispatch({
+    type: 'song/addSong',
+    payload: 'New Song!!!',
+});
+
+const finalState= store.getState();
+console.log(JSON.stringify(finalState));
